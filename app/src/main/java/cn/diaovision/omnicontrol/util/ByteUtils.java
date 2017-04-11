6ed987a39451715e8b334eb35108d0a151e46734
lingfliu <lingf.liu@gmail.com>
2017-04-11 16:33:10 +0800

    1. added int2bytes, bytes2int in big-endian format

--- app/src/main/java/cn/diaovision/omnicontrol/util/ByteUtils.java
+++ app/src/main/java/cn/diaovision/omnicontrol/util/ByteUtils.java
@@ -9,25 +9,22 @@ public class ByteUtils {
     public static byte[] int2bytes(int val, int len){
         byte[] bytes = new byte[len];
         for (int m = 0; m < len; m ++){
-            bytes[m] = (byte) ( (val >>(8*m)) & 0xff );
+            //big-endian
+            bytes[len-m-1] = (byte) ( (val >>(8*m)) & 0xff );
         }
         return bytes;
     }
 
-    public static int bytes2int(byte[] bytes){
+    public static int bytes2int(byte[] bytes, int offset, int len){
+        if (bytes.length < offset || bytes.length < len){
+            return 0;
+        }
+
+        //big-endian
         int val = 0;
         for (int m = 0; m < bytes.length; m ++){
-            val += bytes[m] << (8*m);
+            val += bytes[offset+len-m-1] << (8*m);
         }
         return val;
-    }
-
-    /*简单的异或校验计算*/
-    public static byte checksum (byte[] bytes){
-        byte cs = 0x00;
-        for (byte b : bytes) {
-            cs ^= b;
-        }
-        return cs;
     }
 }
