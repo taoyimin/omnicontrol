63287d4cb672305f2a965793c2567d9b53bf1fd2
lingfliu <lingf.liu@gmail.com>
2017-04-24 14:23:07 +0800

    added config

---
+++ app/src/main/java/cn/diaovision/omnicontrol/model/Config.java
@@ -0,0 +1,13 @@
+package cn.diaovision.omnicontrol.model;
+
+/**
+ * Created by liulingfeng on 2017/4/24.
+ */
+
+public interface Config {
+    String getMainName();
+    String getMainPasswd();
+
+    String getConfName();
+    String getConfPasswd();
+}
