4c411458cbab2d0c376af5144edff1d0864650a8
lingfliu <lingf.liu@gmail.com>
2017-04-25 22:10:10 +0800

    revised

--- app/src/test/java/cn/diaovision/omnicontrol/util/ByteBufferTest.java
+++ app/src/test/java/cn/diaovision/omnicontrol/util/ByteBufferTest.java
@@ -9,37 +9,7 @@ import static org.junit.Assert.*;
  */
 public class ByteBufferTest {
     @Test
-    public void push1() throws Exception {
-
-    }
-
-    @Test
-    public void read1() throws Exception {
-
-    }
-
-    @Test
-    public void pop1() throws Exception {
-
-    }
-
-    @Test
-    public void flush1() throws Exception {
-
-    }
-
-    @Test
-    public void reset1() throws Exception {
-
-    }
-
-    @Test
-    public void getContentLen1() throws Exception {
-
-    }
-
-    @Test
-    public void push() throws Exception {
+    public void crud() throws Exception {
         ByteBuffer byteBuff = new ByteBuffer(10);
 
         byte[] bytes = new byte[14];
@@ -47,35 +17,23 @@ public class ByteBufferTest {
             bytes[m] = (byte)m;
         }
         byteBuff.push(bytes, bytes.length);
+        assertEquals("rw", byteBuff.rw, 4);
 
-        assertEquals("current", byteBuff.c, 3);
-        assertEquals("tail", byteBuff.t, 4);
+        byte[] bs = new byte[10];
+        byteBuff.read(bs, 4);
+        assertEquals("read", bs[0], 4);
+        byteBuff.pop(bs, 4);
+        assertEquals("pop", bs[0], 4);
+        byteBuff.read(bs, 4);
+        assertEquals("read", bs[0], 8);
+        assertEquals("rw", byteBuff.rw, 8);
+        byteBuff.pop(bs, 4);
+        assertEquals("rw", byteBuff.rw, 2);
+        byteBuff.pop(bs, 1);
+        assertEquals("rw", byteBuff.rw, 3);
+
+        assertEquals("current", byteBuff.c, 4);
+        assertEquals("tail", byteBuff.t, 9);
+        assertEquals("contentLen", byteBuff.getContentLen(), 1);
     }
-
-
-    @Test
-    public void read() throws Exception {
-
-    }
-
-    @Test
-    public void pop() throws Exception {
-
-    }
-
-    @Test
-    public void flush() throws Exception {
-
-    }
-
-    @Test
-    public void reset() throws Exception {
-
-    }
-
-    @Test
-    public void getContentLen() throws Exception {
-
-    }
-
 }
