2acf8c4938b64c4e0f8967a07534afd9fb1ab72d
lingfliu <lingf.liu@gmail.com>
2017-04-25 11:40:20 +0800

    revised init(), added stopwork()

--- app/src/main/java/cn/diaovision/omnicontrol/core/model/conference/ConfManager.java
+++ app/src/main/java/cn/diaovision/omnicontrol/core/model/conference/ConfManager.java
@@ -1,7 +1,12 @@
 package cn.diaovision.omnicontrol.core.model.conference;
 
+import org.reactivestreams.Publisher;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,14 +16,21 @@ import cn.diaovision.omnicontrol.core.message.conference.McuMessage;
 import cn.diaovision.omnicontrol.core.message.conference.StreamMediaMessage;
 import cn.diaovision.omnicontrol.core.message.conference.TermInfoMessage;
 import cn.diaovision.omnicontrol.model.Config;
+import cn.diaovision.omnicontrol.rx.RxExecutor;
 import cn.diaovision.omnicontrol.rx.RxMessage;
+import cn.diaovision.omnicontrol.rx.RxReq;
 import cn.diaovision.omnicontrol.rx.RxSubscriber;
+import cn.diaovision.omnicontrol.rx.RxThen;
+import io.reactivex.Flowable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 /**
  * Created by liulingfeng on 2017/4/24.
  */
 
 public class ConfManager {
+    public static final int STATE_IDLE = 0;
     public static final int STATE_PREPARING = 1;
     public static final int STATE_READY = 2;
 
@@ -27,13 +39,14 @@ public class ConfManager {
     Mcu mcu;
     McuCommManager mcuCommMgr;
 
+    Conf conf; //this manager only handles one conf
 
     ConfInfoMessage confInfoTemplate;
 
     Map<Integer, TermInfoMessage> termInfos;
 
     public ConfManager() {
-        this.state = new AtomicInteger(STATE_PREPARING);
+        this.state = new AtomicInteger(STATE_IDLE);
         this.termInfos = new ConcurrentHashMap<>();
     }
 
@@ -45,73 +58,92 @@ public class ConfManager {
         this.state = state;
     }
 
-    public void init(Config cfg){
-//
-//        //1. 初始化commMgr
-//        mcu = new Mcu();
-//        mcu.init(cfg);
-//
-//        mcuCommMgr = new McuCommMgr(mcu);
-//
-//        mcuCommMgr.connect(new RxSubscriber<RxMessage>() {
-//            @Override
-//            public void onRxResult(RxMessage rxMessage) {
-//                //connection error
-//                if (rxMessage.what == RxMessage.DISCONNECTED){
-//                    mcu.state = Mcu.STATE_OFFLINE;
-//                }
-//                else {
-//                     mcu.state = Mcu.STATE_ONLINE;
-//
-//                    //2. 获得会议配置模板
-//                    McuMessage msg = McuMessage.buildReqConfAll();
-//                    mcuCommMgr.send(msg, new RxSubscriber<RxMessage>() {
-//                        @Override
-//                        public void onRxResult(RxMessage rxMessage) {
-//                            //3. 获得term列表
-//                            confInfoTemplate = (ConfInfoMessage) rxMessage.val;
-//                            int confNum = confInfoTemplate.getConfNum();
-//
-//                            for (int m = 0; m < confNum; m ++){
-//                                McuMessage msg = McuMessage.buildReqTermAll(confInfoTemplate.getConfConfig()[m].getId());
-//                                mcuCommMgr.send(msg, new RxSubscriber<RxMessage>() {
-//                                    @Override
-//                                    public void onRxResult(RxMessage rxMsg) {
-//                                        termInfos.put(confInfoTemplate.getConfConfig()[m].getId(), rxMsg.val);
-//                                        state.set(STATE_READY);
-//                                    }
-//                                    @Override
-//                                    public void onRxError(Throwable e) {
-//                                        mcu.state = Mcu.STATE_OFFLINE;
-//                                    }
-//                                });
-//                            }
-//                        }
-//                        @Override
-//                        public void onRxError(Throwable e) {
-//                            mcu.state = Mcu.STATE_OFFLINE;
-//                        }
-//                    });
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onRxError(Throwable e) {
-//                //connection error
-//                mcu.state = Mcu.STATE_OFFLINE;
-//            }
-//        });
+    /*初始化，异步调用*/
+    public void init(Config cfg, RxSubscriber<RxMessage> subscriber){
+
+        //1. 初始化commMgr
+        mcu = new Mcu(cfg.getMcuIp(), cfg.getMcuPort());
+        mcuCommMgr = new McuCommManager(mcu);
+
+        //2. connect mcu, get confTemplate, get confInfo become ready
+        //   if any error -> onError -> state idle
+
+        mcuCommMgr.connect(new RxSubscriber<RxMessage>() {
+                               @Override
+                               public void onRxResult(RxMessage rxMsg) {
+
+                                   McuMessage reqConfInfoMsg = McuMessage.buildReqConfAll();
+
+                                   RxReq reqConfInfo = new RxReq() {
+                                       @Override
+                                       public RxMessage request() {
+                                           //TODO: save conf info
+                                           return null;
+                                       }
+                                   };
+
+                                   McuMessage reqConfTemplateMsg = McuMessage.buildReqConfConfiged();
+
+                                   RxReq reqConfTemplate = new RxReq() {
+                                       @Override
+                                       public RxMessage request() {
+                                           //TODO: save conf template
+                                           return null;
+                                       }
+                                   };
+
+                                   List<McuCommManager.McuBundle> bundleList = new ArrayList<McuCommManager.McuBundle>();
+
+                                   McuCommManager.McuBundle bundle = new McuCommManager.McuBundle();
+                                   bundle.msg = reqConfInfoMsg;
+                                   bundle.req = reqConfInfo;
+                                   bundleList.add(bundle);
+
+                                   bundle = new McuCommManager.McuBundle();
+                                   bundle.msg = reqConfTemplateMsg;
+                                   bundle.req = reqConfTemplate;
+                                   bundleList.add(bundle);
+
+                                   mcuCommMgr.sendSequential(bundleList, new RxSubscriber() {
+                                       @Override
+                                       public void onRxResult(Object o) {
+                                           state.set(STATE_READY);
+                                       }
+
+                                       @Override
+                                       public void onRxError(Throwable e) {
+                                           state.set(STATE_IDLE);
+                                       }
+                                   });
+                               }
+
+                               @Override
+                               public void onRxError(Throwable e) {
+                                   state.set(STATE_IDLE);
+                               }
+                           }
+        );
+    }
+
+    /*退出当前UI请务必调用这个方法*/
+    public void stopWork(){
+        mcuCommMgr.disconnect();
     }
 
     /*获得会议的全部配置模板，这里不需要单独获得某个会议的模板*/
-    private void reqConfTemplate(int confId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqConf(confId);
+    private void reqConfTemplate(RxSubscriber<RxMessage> subscriber){
+        McuMessage msg = McuMessage.buildReqConfConfiged();
+        mcuCommMgr.send(msg, subscriber);
+    }
+
+    /*获得所有的会议, 不需要单独获得某个会议的模板*/
+    private void reqConfInfo(RxSubscriber<RxMessage> subscriber){
+        McuMessage msg = McuMessage.buildReqConfAll();
         mcuCommMgr.send(msg, subscriber);
     }
 
     /*hangup*/
-    public void handup(int confId, int termId, RxSubscriber<RxMessage> subscriber){
+    public void handupTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
         McuMessage msg = McuMessage.buildReqHangupTerm(confId, termId);
         mcuCommMgr.send(msg, subscriber);
     }
@@ -121,8 +153,6 @@ public class ConfManager {
      * confId: where termList belongs
      ***********************************/
     public int startConf(Date startTime, Date endTime, int confId, RxSubscriber<RxMessage> subscriber){
-        //TODO: return -1 if no template found
-
         ConfConfigMessage confConfigMessage = null;
         if (confInfoTemplate == null)
             return -1;
@@ -145,76 +175,192 @@ public class ConfManager {
         return 0;
     }
 
-    public void endConf(int confId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqDeleteConf(confId);
-        mcuCommMgr.send(msg, subscriber);
+    public int endConf(int confId, RxSubscriber<RxMessage> subscriber){
+
+        if (confIdValid(confId)){
+            McuMessage msg = McuMessage.buildReqDeleteConf(confId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        } else {
+            return -1;
+        }
+
+
     }
 
-    public void inviteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqInviteTerm(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int inviteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)){
+            McuMessage msg = McuMessage.buildReqInviteTerm(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
-    public void hangupTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqHangupTerm(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int hangupTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqHangupTerm(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        } else {
+            return -1;
+        }
+
     }
 
-    public void muteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqMute(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int muteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqMute(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
-    public void unmuteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqUnmute(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int unmuteTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqUnmute(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        } else {
+            return -1;
+        }
     }
 
     /*点名发言*/
-    public void speechTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqTermSpeach(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int speechTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqTermSpeach(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
+
     }
 
     /*取消点名发言*/
-    public void cancelSpeechTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqTermCancelSpeach(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int cancelSpeechTerm(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqTermCancelSpeach(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        } else {
+            return -1;
+        }
     }
 
 
-    public void makeChair(int confId, long termId, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqTermBroadcast(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int makeChair(int confId, long termId, RxSubscriber<RxMessage> subscriber){
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqTermBroadcast(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        } else {
+            return -1;
+        }
     }
 
-    public void makeSelectview(int confId, long termId, RxSubscriber<RxMessage> subscriber) {
-        McuMessage msg = McuMessage.buildReqTermSelectView(confId, termId);
-        mcuCommMgr.send(msg, subscriber);
+    public int makeSelectview(int confId, long termId, RxSubscriber<RxMessage> subscriber) {
+        if (termIdValid(confId, termId)) {
+            McuMessage msg = McuMessage.buildReqTermSelectView(confId, termId);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     /*打开主席花蜜那*/
-    public void openChairStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.OPEN_SERVER, localIp);
-        mcuCommMgr.send(msg, subscriber);
+    public int openChairStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
+        if (confIdValid(confId)) {
+            McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.OPEN_SERVER, localIp);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     /*关闭主席画面*/
-    public void closeChairStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.CLOSE_SERVER, localIp);
-        mcuCommMgr.send(msg, subscriber);
+    public int closeChairStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
+        if (confIdValid(confId)) {
+            McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.CLOSE_SERVER, localIp);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     /*打开选看画面*/
-    public void openSelectviewStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.OPEN_SELECTVIEW, localIp);
-        mcuCommMgr.send(msg, subscriber);
+    public int openSelectviewStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
+        if (confIdValid(confId)) {
+            McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.OPEN_SELECTVIEW, localIp);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     /*关闭选看画面*/
-    public void closeSelectviewStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
-        McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.CLOSE_SELECTVIEW, localIp);
-        mcuCommMgr.send(msg, subscriber);
+    public int closeSelectviewStream(int confId, String localIp, RxSubscriber<RxMessage> subscriber){
+        if (confIdValid(confId)) {
+            McuMessage msg = McuMessage.buildReqStream(confId, StreamMediaMessage.CLOSE_SELECTVIEW, localIp);
+            mcuCommMgr.send(msg, subscriber);
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+    public boolean confIdValid(int confId){
+        if (confInfoTemplate == null)
+            return false;
+
+        boolean idValid = false;
+        for (int m = 0; m < confInfoTemplate.getConfNum(); m ++){
+            if (confInfoTemplate.getConfConfig()[m].getId() == confId){
+                idValid = true;
+                break;
+            }
+        }
+
+        return idValid;
+    }
+
+    public boolean termIdValid(int confId, long termId){
+        boolean idValid = false;
+        if (confIdValid(confId)){
+            for (int m = 0; m < confInfoTemplate.getConfNum(); m ++){
+                if (confInfoTemplate.getConfConfig()[m].getId() == confId){
+                    for (int n = 0; n < confInfoTemplate.getConfConfig()[m].getTermAttrNum(); n ++){
+                        if (confInfoTemplate.getConfConfig()[m].getTermAttrs()[n].id == termId){
+                            idValid = true;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            return idValid;
+        }
+        else {
+            return idValid;
+        }
     }
 }
 
