a4a8bd8598a6fa4686b580a24b44d6d75775f61f
taoyimin <1103995288@qq.com>
2017-04-26 17:26:36 +0800

    添加item长按事件与recyclerview分发事件的回掉接口

--- app/src/main/java/cn/diaovision/omnicontrol/widget/PortRadioGroupView.java
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/PortRadioGroupView.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.List;
@@ -24,21 +25,20 @@ public class PortRadioGroupView extends RecyclerView {
     public PortItemAdapter adapter;
 
     OnItemSelectListener onItemSelectListener;
-
     OnItemLongClickListener onItemLongClickListener;
+    DispatchTouchEventListener dispatchTouchEventListener;
 
     boolean configed = false;
     boolean dragging = false;
     boolean scrolling = false;
 
 
-
     public PortRadioGroupView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        ctx=context;
+        ctx = context;
     }
 
-    public void config(final List<Port> portList, int layout){
+    public void config(final List<Port> portList, int layout) {
         this.portList = portList;
         this.layout = layout;
 
@@ -54,21 +54,21 @@ public class PortRadioGroupView extends RecyclerView {
             public void onLongClick(View v, int position) {
                 //弹出对话框
                 //popupDialog(portList.get(position));
-                if (onItemLongClickListener != null){
-                    onItemLongClickListener.onLongClick(v,position);
+                if (onItemLongClickListener != null) {
+                    onItemLongClickListener.onLongClick(v, position);
                 }
             }
 
             @Override
             public void onSelect(View v, int position) {
-                if (onItemSelectListener != null){
+                if (onItemSelectListener != null) {
                     onItemSelectListener.onSelected(position);
                 }
             }
 
             @Override
             public void onUnselect(View v, int position) {
-                if (onItemSelectListener != null){
+                if (onItemSelectListener != null) {
                     onItemSelectListener.onUnselected(position);
                 }
 
@@ -79,21 +79,21 @@ public class PortRadioGroupView extends RecyclerView {
 
     }
 
-    public void select(final int position){
+    public void select(final int position) {
         if (!configed)
             return;
 
         int firstPosition = ((GridLayoutManager) this.getLayoutManager()).findFirstVisibleItemPosition();
         int lastPosition = ((GridLayoutManager) this.getLayoutManager()).findLastVisibleItemPosition();
-        if (position < firstPosition || position > lastPosition){
+        if (position < firstPosition || position > lastPosition) {
             this.smoothScrollToPosition(position);
             this.addOnScrollListener(new RecyclerView.OnScrollListener() {
                 @Override
                 public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                     super.onScrollStateChanged(recyclerView, newState);
-                    switch(newState){
+                    switch (newState) {
                         case RecyclerView.SCROLL_STATE_SETTLING:
-                            if (!dragging && !scrolling){
+                            if (!dragging && !scrolling) {
                                 scrolling = true; //a scrolling occurs
                             }
                             break;
@@ -101,16 +101,15 @@ public class PortRadioGroupView extends RecyclerView {
                             dragging = true;
                             break;
                         case RecyclerView.SCROLL_STATE_IDLE:
-                            if (!dragging && scrolling){
+                            if (!dragging && scrolling) {
                                 dragging = false;
                                 scrolling = false;
                                 //N.B.: firstVisibleItemPosition is not the first child of layoutmanager
-                                adapter.changeSelectedItem(position, layoutMgr.getChildAt(position-(int) layoutMgr.getChildAt(0).getTag()));
+                                adapter.changeSelectedItem(position, layoutMgr.getChildAt(position - (int) layoutMgr.getChildAt(0).getTag()));
                                 int firstPosition = layoutMgr.findFirstVisibleItemPosition();
                                 int lastPosition = layoutMgr.findLastVisibleItemPosition();
 
-                            }
-                            else {
+                            } else {
                                 //dragging should be unset once the scroll is done
                                 dragging = false;
                             }
@@ -124,20 +123,28 @@ public class PortRadioGroupView extends RecyclerView {
                 }
             });
 
-        }
-        else {
+        } else {
             //a hotfix for layoutmanager.getChildAt
-            adapter.changeSelectedItem(position, layoutMgr.getChildAt(position-(int) layoutMgr.getChildAt(0).getTag()));
+            adapter.changeSelectedItem(position, layoutMgr.getChildAt(position - (int) layoutMgr.getChildAt(0).getTag()));
         }
     }
 
     public interface OnItemSelectListener {
         void onSelected(int pos);
+
         void onUnselected(int pos);
     }
 
-    public interface OnItemLongClickListener{
-        void onLongClick(View v,int position);
+    public interface OnItemLongClickListener {
+        void onLongClick(View v, int position);
+    }
+
+    public interface DispatchTouchEventListener {
+        void dispatchTouchEvent(View v, MotionEvent e, int position);
+    }
+
+    public void setDispatchTouchEventListener(DispatchTouchEventListener dispatchTouchEventListener) {
+        this.dispatchTouchEventListener = dispatchTouchEventListener;
     }
 
     public void setOnItemSelectListener(OnItemSelectListener onItemSelectListener) {
@@ -148,8 +155,9 @@ public class PortRadioGroupView extends RecyclerView {
         this.onItemLongClickListener = onItemLongClickListener;
     }
 
+
     /*when data changed*/
-    public void updateData(){
+    public void updateData() {
         if (!configed)
             return;
 
@@ -160,12 +168,25 @@ public class PortRadioGroupView extends RecyclerView {
         return portList;
     }
 
-    public void    setPortList(List<Port> portList) {
+    public void setPortList(List<Port> portList) {
         this.portList = portList;
     }
 
-    public void configLayout(int direction, int spancount){
+    public void configLayout(int direction, int spancount) {
         layoutMgr = new GridLayoutManager(ctx, spancount, direction, false);
         this.setLayoutManager(layoutMgr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(final MotionEvent ev) {
+        if (dispatchTouchEventListener != null) {
+            adapter.setOnItemTouchListener(new PortItemAdapter.OnItemTouchListener() {
+                @Override
+                public void onItemTouchEvent(View v, int position) {
+                    dispatchTouchEventListener.dispatchTouchEvent(v, ev, position);
+                }
+            });
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
