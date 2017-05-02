cc158ffe4049eb3113a02b86c151845ab8bd5e3a
taoyimin <1103995288@qq.com>
2017-05-02 19:48:16 +0800

    增加获取截屏文件夹方法

--- app/src/main/java/cn/diaovision/omnicontrol/util/FileHelper.java
+++ app/src/main/java/cn/diaovision/omnicontrol/util/FileHelper.java
@@ -1,8 +1,19 @@
 package cn.diaovision.omnicontrol.util;
 
+import android.os.Environment;
+
 /**
  * Created by liulingfeng on 2017/3/2.
  */
 
 public class FileHelper {
+    public static String getSrceenShotPath(){
+        String path="";
+        //判断SD卡是否挂载
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)){
+            path=Environment.getExternalStorageDirectory().getAbsolutePath()+"/Omnicontrol/screenshot";
+        }
+        return path;
+    }
 }
