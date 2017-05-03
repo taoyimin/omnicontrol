5caab93fe14e5b61c53b65ae9bcf147d9c0c763f
taoyimin <1103995288@qq.com>
2017-05-03 17:28:03 +0800

    添加了列表多选编辑功能

--- app/src/main/java/cn/diaovision/omnicontrol/widget/adapter/PortItemAdapter.java
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/adapter/PortItemAdapter.java
@@ -8,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -21,7 +22,7 @@ import cn.diaovision.omnicontrol.widget.CircleCharView;
  * Created by liulingfeng on 2017/3/2.
  */
 
-public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortItemViewHolder>{
+public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortItemViewHolder> {
 
     List<Port> ports;
     int layout;
@@ -37,7 +38,11 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
     private OnItemClickListener itemClickListener;
     OnItemTouchListener onItemTouchListener;
 
-    public PortItemAdapter(List<Port> ports, int layout){
+    private boolean isEditing = false;
+    //存放选中item的position
+    List<Integer> selects = new ArrayList<>();
+
+    public PortItemAdapter(List<Port> ports, int layout) {
         this.ports = ports;
         this.layout = layout;
         isBinding = new AtomicBoolean(false);
@@ -46,8 +51,6 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
     @Override
     public PortItemViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
-
-
         PortItemViewHolder vholder = new PortItemViewHolder(v);
         ctx = parent.getContext();
         return vholder;
@@ -61,7 +64,7 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
         holder.getV().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (lastSelectedPos < 0){
+                /*if (lastSelectedPos < 0) {
                     //new select
                     lastSelectedPos = holder.pos;
                     ((CircleCharView) view.findViewById(R.id.port_circle)).select();
@@ -69,12 +72,10 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
                     if (itemClickListener != null) {
                         itemClickListener.onSelect(view, (int) view.getTag());
                     }
-                }
-                else {
+                } else {
                     if (lastSelectedPos != holder.pos) {
-
                         //only unselect old selectedView if the view is still on the window
-                        if((int) lastSelectedView.getTag() == lastSelectedPos) {
+                        if ((int) lastSelectedView.getTag() == lastSelectedPos) {
                             ((CircleCharView) lastSelectedView.findViewById(R.id.port_circle)).unselect();
                         }
                         lastSelectedPos = holder.pos;
@@ -85,8 +86,7 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
                         if (itemClickListener != null) { //callback
                             itemClickListener.onSelect(view, (int) view.getTag());
                         }
-                    }
-                    else {
+                    } else {
                         //unselect
                         lastSelectedPos = -1;
                         ((CircleCharView) view.findViewById(R.id.port_circle)).unselect();
@@ -95,16 +95,74 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
                             itemClickListener.onUnselect(view, (int) view.getTag());
                         }
                     }
-                }
+                }*/
 
+                switch (selects.size()) {
+                    case 0:
+                        //之前没有选中
+                        selects.add(position);
+                        ((CircleCharView) view.findViewById(R.id.port_circle)).select();
+                        lastSelectedPos = holder.pos;
+                        lastSelectedView = view;
+                        if (itemClickListener != null) {
+                            itemClickListener.onSelect(view, (int) view.getTag());
+                        }
+                        break;
+                    case 1:
+                        //之前选中一个
+                        if (selects.get(0) != holder.pos) {
+                            //选中的和之前不同
+                            if (isEditing) {
+                                selects.add(position);
+                                ((CircleCharView) view.findViewById(R.id.port_circle)).select();
+                                lastSelectedPos = holder.pos;
+                                lastSelectedView = view;
+                            } else {
+                                selects.remove(0);
+                                selects.add(position);
+                                if ((int) lastSelectedView.getTag() == lastSelectedPos) {
+                                    ((CircleCharView) lastSelectedView.findViewById(R.id.port_circle)).unselect();
+                                }
+                                ((CircleCharView) view.findViewById(R.id.port_circle)).select();
+                                lastSelectedPos = holder.pos;
+                                lastSelectedView = view;
+                            }
+                        } else {
+                            //选中的和之前相同
+                            selects.remove(0);
+                            ((CircleCharView) view.findViewById(R.id.port_circle)).unselect();
+                        }
+                        break;
+                    default:
+                        //之前选中多个
+                        if (isEditing) {
+                            if (selects.contains(position)) {
+                                selects.remove(selects.indexOf(position));
+                                ((CircleCharView) view.findViewById(R.id.port_circle)).unselect();
+                            } else {
+                                selects.add(position);
+                                ((CircleCharView) view.findViewById(R.id.port_circle)).select();
+                                lastSelectedPos = holder.pos;
+                                lastSelectedView = view;
+                            }
+                        } else {
+                            selects.clear();
+                            notifyDataSetChanged();
+                            selects.add(position);
+                            ((CircleCharView) view.findViewById(R.id.port_circle)).select();
+                            lastSelectedPos = holder.pos;
+                            lastSelectedView = view;
+                        }
+                        break;
+                }
             }
         });
 
         holder.getV().setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                if(itemClickListener!=null){
-                    itemClickListener.onLongClick(v,position);
+                if (itemClickListener != null) {
+                    itemClickListener.onLongClick(v, position);
                 }
                 return true;
             }
@@ -113,19 +171,19 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
         holder.getV().setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(onItemTouchListener!=null){
-                    onItemTouchListener.onItemTouchEvent(v,position);
+                if (onItemTouchListener != null) {
+                    onItemTouchListener.onItemTouchEvent(v, position);
                 }
                 return false;
             }
         });
 
         //设置badgeview
-        holder.image.showTextBadge(ports.get(position).idx+"");
+        holder.image.showTextBadge(ports.get(position).idx + "");
         holder.image.isShowBadge();
 
         //bind view here
-        if (ports != null){
+        if (ports != null) {
             Port p = ports.get(position);
             holder.alias.setText(p.alias);
             holder.cView.changeState(p.state);
@@ -133,11 +191,17 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
             holder.pos = position;
 
             //reset the view click state
-            if (lastSelectedPos == position){
+/*            if (lastSelectedPos == position) {
                 holder.cView.select(0);
                 lastSelectedView = holder.getV(); //reset selectedView here (old one may be recycled)
-            }
-            else {
+            } else {
+                holder.cView.unselect(0);
+            }*/
+
+            if (selects.contains(position)) {
+                holder.cView.select(0);
+                lastSelectedView = holder.getV();
+            } else {
                 holder.cView.unselect(0);
             }
             holder.v.setTag(position); //view position
@@ -146,7 +210,7 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
         isBinding.set(false);
     }
 
-    public void setOnItemClickedListener(OnItemClickListener listener){
+    public void setOnItemClickedListener(OnItemClickListener listener) {
         this.itemClickListener = listener;
     }
 
@@ -161,16 +225,16 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
     }
 
     public void setPorts(List<Port> ports) {
-        if(ports != null) {
+        if (ports != null) {
             this.ports = ports;
             notifyDataSetChanged();
         }
     }
 
-    public void changeSelectedItem(int pos, View view){
-        if (lastSelectedPos != pos){
+    public void changeSelectedItem(int pos, View view) {
+        if (lastSelectedPos != pos) {
             ((CircleCharView) view.findViewById(R.id.port_circle)).select();
-            if (lastSelectedView !=null && (int) lastSelectedView.getTag() == lastSelectedPos) {
+            if (lastSelectedView != null && (int) lastSelectedView.getTag() == lastSelectedPos) {
                 ((CircleCharView) lastSelectedView.findViewById(R.id.port_circle)).unselect();
             }
             lastSelectedPos = pos;
@@ -178,7 +242,7 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
         }
     }
 
-    public class PortItemViewHolder extends RecyclerView.ViewHolder{
+    public class PortItemViewHolder extends RecyclerView.ViewHolder {
         View v;
         AppCompatTextView alias;
         CircleCharView cView;
@@ -197,11 +261,11 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
 
             alias = (AppCompatTextView) v.findViewById(R.id.port_alias);
             cView = (CircleCharView) v.findViewById(R.id.port_circle);
-            image= (BGABadgeImageView) v.findViewById(R.id.port_image);
+            image = (BGABadgeImageView) v.findViewById(R.id.port_image);
             cView.unselect(0);
         }
 
-        public View getV(){
+        public View getV() {
             return v;
         }
 
@@ -210,17 +274,30 @@ public class PortItemAdapter extends RecyclerView.Adapter<PortItemAdapter.PortIt
         }
     }
 
-    public interface OnItemClickListener{
+    public interface OnItemClickListener {
         void onLongClick(View v, int position);
+
         void onSelect(View v, int position);
+
         void onUnselect(View v, int position);
     }
 
-    public interface OnItemTouchListener{
+    public interface OnItemTouchListener {
         void onItemTouchEvent(View v, int position);
     }
 
     public void setOnItemTouchListener(OnItemTouchListener onItemTouchListener) {
         this.onItemTouchListener = onItemTouchListener;
+    }
+
+    public boolean isEditing() {
+        return isEditing;
+    }
+
+    public void setEditing(boolean editing) {
+        if (selects != null && editing == true) {
+            selects.clear();
+        }
+        isEditing = editing;
     }
 }
