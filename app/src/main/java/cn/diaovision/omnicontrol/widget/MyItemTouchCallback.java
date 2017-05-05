e205ada712c9ee6a6cb5d1cfbc092d32aa4b86eb
taoyimin <1103995288@qq.com>
2017-05-05 09:58:02 +0800

    拖拽的ItemTouchCallback

---
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/MyItemTouchCallback.java
@@ -0,0 +1,63 @@
+package cn.diaovision.omnicontrol.widget;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import java.util.Collections;
+
+import cn.diaovision.omnicontrol.widget.adapter.AuxiliaryPanelItemAdapter;
+
+/**
+ * Created by TaoYimin on 2017/5/5.
+ */
+
+public class MyItemTouchCallback extends ItemTouchHelper.Callback {
+    AuxiliaryPanelItemAdapter adapter;
+
+    public MyItemTouchCallback(AuxiliaryPanelItemAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        if (recyclerView.getLayoutManager() instanceof GridLayoutManager) {
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        final int swipeFlags = 0;
+        return makeMovementFlags(dragFlags, swipeFlags);
+        } else {
+            final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+            final int swipeFlags = 0;
+            return makeMovementFlags(dragFlags, swipeFlags);
+        }
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                          RecyclerView.ViewHolder target) {
+        viewHolder.itemView.bringToFront();
+        int fromPosition = viewHolder.getAdapterPosition();//得到拖动ViewHolder的position
+        int toPosition = target.getAdapterPosition();//得到目标ViewHolder的position
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(adapter.getList(), i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(adapter.getList(), i, i - 1);
+            }
+        }
+        adapter.notifyItemMoved(fromPosition, toPosition);
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return false;
+    }
+}
