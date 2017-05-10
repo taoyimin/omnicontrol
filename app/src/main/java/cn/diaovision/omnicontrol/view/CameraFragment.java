189cd7bff40fae8d0545046c2589a18e4161345c
taoyimin <1103995288@qq.com>
2017-05-10 17:16:03 +0800

    添加了控制按钮的触摸事件监听

--- app/src/main/java/cn/diaovision/omnicontrol/view/CameraFragment.java
+++ app/src/main/java/cn/diaovision/omnicontrol/view/CameraFragment.java
@@ -6,14 +6,17 @@ import android.support.v7.widget.GridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import cn.diaovision.omnicontrol.BaseFragment;
 import cn.diaovision.omnicontrol.R;
@@ -47,6 +50,9 @@ public class CameraFragment extends BaseFragment implements CameraContract.View{
     @BindView(R.id.video_layout)
     VideoLayout videoLayout;
 
+    @BindViews({R.id.btn_zoomin,R.id.btn_zoomout,R.id.btn_fast,R.id.btn_slow,R.id.btn_stop})
+    List<Button> btnList;
+
     CameraPresenter presenter;
 
     int lastDeg;
@@ -70,6 +76,7 @@ public class CameraFragment extends BaseFragment implements CameraContract.View{
         presenter = new CameraPresenter(this);
         /* test code */
         final List<Preset> presetList = new ArrayList<>();
+        //final List<Preset> presetList = currentCamera.getPresetList();
         final List<Port> portList = new ArrayList<>();
 
         for (int m = 0; m < 12; m ++){
@@ -156,9 +163,43 @@ public class CameraFragment extends BaseFragment implements CameraContract.View{
             }
         });
 
+        for(Button btn:btnList){
+            btn.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            switch (v.getId()){
+                                case R.id.btn_zoomin:
+                                    presenter.cameraCtrlGo(currentCamera.getPortIdx(),MatrixMessage.CAM_ZOOMIN, 63);
+                                    break;
+                                case R.id.btn_zoomout:
+                                    presenter.cameraCtrlGo(currentCamera.getPortIdx(),MatrixMessage.CAM_ZOOMOUT, 63);
+                                    break;
+                                case R.id.btn_fast:
+                                    break;
+                                case R.id.btn_slow:
+                                    break;
+                                case R.id.btn_stop:
+                                    break;
+                            }
+                            break;
+                        case MotionEvent.ACTION_UP:
+                        case MotionEvent.ACTION_CANCEL:
+                        case MotionEvent.ACTION_HOVER_EXIT:
+                            presenter.cameraStopGo(currentCamera.getPortIdx());
+                            break;
+                        default:
+                            break;
+                    }
+                    return false;
+                }
+            });
+        }
+
         //设置播放路径
         //videoLayout.setVideoPath("rtsp://192.168.10.108:8554/test.mov");
-        videoLayout.setVideoPath("rtsp://192.168.10.31:554/test1.MP4");
+        //videoLayout.setVideoPath("rtsp://192.168.10.31:554/test1.MP4");
         //videoLayout.setVideoPath("http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8");
         //videoLayout.setVideoPath("http://live.3gv.ifeng.com/live/zixun.m3u8");
         //videoLayout.setVideoPath("rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp");
