0ec567019da253661dcc222e045c2946d5787e02
taoyimin <1103995288@qq.com>
2017-05-23 17:43:23 +0800

    add vertical viewpager

---
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/VerticalViewPager.java
@@ -0,0 +1,47 @@
+package cn.diaovision.omnicontrol.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by TaoYimin on 2017/5/22.
+ */
+public class VerticalViewPager extends ViewPager {
+
+    public VerticalViewPager(Context context) {
+        this(context, null);
+    }
+
+    public VerticalViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPageTransformer(false, new ViewPagerTransformer());
+    }
+
+    private MotionEvent swapTouchEvent(MotionEvent event) {
+        float width = getWidth();
+        float height = getHeight();
+
+        float swappedX = (event.getY() / height) * width;
+        float swappedY = (event.getX() / width) * height;
+
+        event.setLocation(swappedX, swappedY);
+
+        return event;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        boolean intercept = super.onInterceptTouchEvent(swapTouchEvent(event));
+        //If not intercept, touch event should not be swapped.
+        swapTouchEvent(event);
+        return intercept;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(swapTouchEvent(ev));
+    }
+
+}
