5f75d838daa9071abdfa6ad03b82a30bf5979a1b
taoyimin <1103995288@qq.com>
2017-05-26 17:51:22 +0800

    regular update

--- app/src/main/java/cn/diaovision/omnicontrol/widget/adapter/SelectableAdapter.java
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/adapter/SelectableAdapter.java
@@ -1,7 +1,6 @@
 package cn.diaovision.omnicontrol.widget.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
