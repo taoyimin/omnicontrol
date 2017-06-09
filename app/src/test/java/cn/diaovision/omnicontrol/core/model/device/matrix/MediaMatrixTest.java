edda6507e972b6765002a3d91efd86c758f6d7cf
lingfliu <lingf.liu@gmail.com>
2017-06-09 16:55:25 +0800

    1. update hashCode for Channel, updateChannel in MediaMatrix works normal 2. junit tests added

---
+++ app/src/test/java/cn/diaovision/omnicontrol/core/model/device/matrix/MediaMatrixTest.java
@@ -0,0 +1,33 @@
+package cn.diaovision.omnicontrol.core.model.device.matrix;
+
+import org.junit.Test;
+
+import cn.diaovision.omnicontrol.core.model.device.matrix.io.Channel;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by liulingfeng on 2017/6/9.
+ */
+public class MediaMatrixTest {
+    @Test
+    public void updateChannelTest() throws Exception {
+        MediaMatrix matrix = new MediaMatrix();
+        int in = 1;
+        int[] outs = {1, 2};
+        Channel chn = new Channel(Channel.CHN_VIDEO, in, outs);
+        matrix.videoChnSet.add(chn);
+
+        in = 2;
+        outs = new int[2];
+        outs[0] = 1;
+        outs[1] = 2;
+        Channel chnUpdate = new Channel(Channel.CHN_VIDEO, in, outs);
+
+        matrix.updateChannel(in, outs, Channel.MOD_NORMAL);
+        for (Channel c : matrix.videoChnSet){
+            assertNotEquals(chn.hashCode(), c.hashCode());
+        }
+    }
+
+}
---
+++ app/src/test/java/cn/diaovision/omnicontrol/core/model/device/matrix/io/ChannelTest.java
@@ -0,0 +1,29 @@
+package cn.diaovision.omnicontrol.core.model.device.matrix.io;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by liulingfeng on 2017/6/9.
+ */
+public class ChannelTest {
+    @Test
+    public void hashCodeTest() throws Exception {
+        int[] outs = new int[2];
+        outs[0] =  1;
+        outs[1] =  2;
+        int in = 1;
+        Channel chn1 = new Channel(Channel.CHN_VIDEO, in, outs);
+
+        outs = new int[2];
+
+        outs[0] =  1;
+        outs[1] =  2;
+        in = 1;
+        Channel chn2 = new Channel(Channel.CHN_VIDEO, in, outs);
+
+        assertEquals(chn1.hashCode(), chn2.hashCode());
+    }
+
+}
