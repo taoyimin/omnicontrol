616485b066a4bf2c26f7611b1331b9959e2a7a8c
taoyimin <1103995288@qq.com>
2017-07-12 17:40:30 +0800

    delete the TermItemAdapter field

--- app/src/main/java/cn/diaovision/omnicontrol/widget/TermItemTouchCallback.java
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/TermItemTouchCallback.java
@@ -3,22 +3,21 @@ package cn.diaovision.omnicontrol.widget;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
-import cn.diaovision.omnicontrol.widget.adapter.TermItemAdapter;
-
 /**
  * Created by TaoYimin on 2017/5/5.
  */
 
 public class TermItemTouchCallback extends ItemTouchHelper.Callback {
-    TermItemAdapter adapter;
+    private OnDragListener onDragListener;
 
-    public TermItemTouchCallback(TermItemAdapter adapter) {
-        this.adapter = adapter;
+    public TermItemTouchCallback() {
     }
 
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        //可以上下左右拖拽
         final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        //不需要滑动
         final int swipeFlags = 0;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
@@ -26,47 +25,37 @@ public class TermItemTouchCallback extends ItemTouchHelper.Callback {
     @Override
     public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
                           RecyclerView.ViewHolder target) {
-/*        viewHolder.itemView.bringToFront();
-        int fromPosition = viewHolder.getAdapterPosition();//得到拖动ViewHolder的position
-        int toPosition = target.getAdapterPosition();//得到目标ViewHolder的position
-        if (fromPosition < toPosition) {
-            for (int i = fromPosition; i < toPosition; i++) {
-                Collections.swap(adapter.getList(), i, i + 1);
-            }
-        } else {
-            for (int i = fromPosition; i > toPosition; i--) {
-                Collections.swap(adapter.getList(), i, i - 1);
-            }
-        }
-        adapter.notifyItemMoved(fromPosition, toPosition);*/
+        //拖动过程中不需要交换item位置，直接返回false
         return false;
     }
 
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-
+        //不需要滑动删除效果
     }
 
     @Override
     public boolean isLongPressDragEnabled() {
+        //需要根据item的位置判断是否可以拖拽，所以返回false
         return false;
     }
 
     @Override
     public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         super.clearView(recyclerView, viewHolder);
+        //拖拽结束后，将完成拖拽的viewHolder回调出去
         if (onDragListener != null) {
             onDragListener.onFinishDrag(viewHolder);
         }
     }
 
-    private OnDragListener onDragListener;
-
+    /*设置拖拽完成的回调*/
     public TermItemTouchCallback setOnDragListener(OnDragListener onDragListener) {
         this.onDragListener = onDragListener;
         return this;
     }
 
+    /*拖拽完成的回调接口*/
     public interface OnDragListener {
         void onFinishDrag(RecyclerView.ViewHolder viewHolder);
     }
