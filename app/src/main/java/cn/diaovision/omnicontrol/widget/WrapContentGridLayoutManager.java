84fc034174d646f7d6251fbcb7ca27cad67e23f3
taoyimin <1103995288@qq.com>
2017-07-20 16:27:52 +0800

    catch exception

---
+++ app/src/main/java/cn/diaovision/omnicontrol/widget/WrapContentGridLayoutManager.java
@@ -0,0 +1,33 @@
+package cn.diaovision.omnicontrol.widget;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by TaoYimin on 2017/7/20.
+ */
+
+public class WrapContentGridLayoutManager extends GridLayoutManager {
+
+    public WrapContentGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public WrapContentGridLayoutManager(Context context, int spanCount) {
+        super(context, spanCount);
+    }
+
+    public WrapContentGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (IndexOutOfBoundsException e) {
+        }
+    }
+}
