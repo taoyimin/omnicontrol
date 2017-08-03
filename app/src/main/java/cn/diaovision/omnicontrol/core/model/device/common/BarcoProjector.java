46b0d09395097813c5f692c8460e9b04a2aea332
taoyimin <1103995288@qq.com>
2017-08-03 18:15:04 +0800

    implement abstract method

--- app/src/main/java/cn/diaovision/omnicontrol/core/model/device/common/BarcoProjector.java
+++ app/src/main/java/cn/diaovision/omnicontrol/core/model/device/common/BarcoProjector.java
@@ -15,6 +15,7 @@ public class BarcoProjector extends CommonDevice{
     /*生成打开电源消息*/
     @Override
     public byte[] buildPowerOnMessage() {
+        //:(space)POWR1(space)(carriage return)
         byte[] bytes=new byte[9];
         bytes[0]=58;
         bytes[1]=32;
@@ -31,6 +32,7 @@ public class BarcoProjector extends CommonDevice{
     /*生成关闭电源消息*/
     @Override
     public byte[] buildPowerOffMessage() {
+        //:(space)POWR0(space)(carriage return)
         byte[] bytes=new byte[9];
         bytes[0]=58;
         bytes[1]=32;
@@ -39,6 +41,23 @@ public class BarcoProjector extends CommonDevice{
         bytes[4]=87;
         bytes[5]=82;
         bytes[6]=48;
+        bytes[7]=32;
+        bytes[8]=13;
+        return bytes;
+    }
+
+    /*生成获取运行状态的消息*/
+    @Override
+    public byte[] buildStateMessage() {
+        //:(space)POST?(space)(carriage return)
+        byte[] bytes=new byte[9];
+        bytes[0]=58;
+        bytes[1]=32;
+        bytes[2]=80;
+        bytes[3]=79;
+        bytes[4]=87;
+        bytes[5]=82;
+        bytes[6]=63;
         bytes[7]=32;
         bytes[8]=13;
         return bytes;
