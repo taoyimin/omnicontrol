86e9e8bb6819c97dc56645033b578aee0adaabe7
taoyimin <1103995288@qq.com>
2017-09-11 14:38:53 +0800

    add the logic of switch audio and show audio spectrum

--- app/src/main/java/cn/diaovision/omnicontrol/view/AudioFragment.java
+++ app/src/main/java/cn/diaovision/omnicontrol/view/AudioFragment.java
@@ -1,6 +1,8 @@
 package cn.diaovision.omnicontrol.view;
 
 import android.app.Service;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,53 +15,51 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
-import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.diaovision.omnicontrol.BaseFragment;
 import cn.diaovision.omnicontrol.R;
-import cn.diaovision.omnicontrol.core.model.device.matrix.io.Channel;
 import cn.diaovision.omnicontrol.core.model.device.matrix.io.Port;
-import cn.diaovision.omnicontrol.widget.AssistDrawerLayout;
+import cn.diaovision.omnicontrol.widget.AudioDrawerLayout;
 import cn.diaovision.omnicontrol.widget.ItemSelectionSupport;
 import cn.diaovision.omnicontrol.widget.OnRecyclerItemClickListener;
+import cn.diaovision.omnicontrol.widget.VisualizerView;
 import cn.diaovision.omnicontrol.widget.adapter.VolumeAdapter;
 
 /**
  * Created by liulingfeng on 2017/2/24.
  */
 
-public class AudioFragment extends BaseFragment implements AudioContract.View{
-
-/*    @BindView(R.id.input)
-    PortRadioGroupView inputList;
-    @BindView(R.id.output)
-    PortRadioGroupView outputList;
-    @BindView(R.id.audioChnList)
-    VolumeChannelRadioGroupView volumeChannelList;*/
-
+public class AudioFragment extends BaseFragment implements AudioContract.View {
     @BindView(R.id.input)
     RecyclerView inputRecyclerView;
     @BindView(R.id.output)
     RecyclerView outputRecyclerView;
     @BindView(R.id.assist_drawer_layout)
-    AssistDrawerLayout drawerLayout;
+    AudioDrawerLayout drawerLayout;
+    @BindView(R.id.visual_view)
+    VisualizerView visualizerView;
 
     private VolumeAdapter inputAdapter;
     private VolumeAdapter outputAdapter;
     private ItemSelectionSupport inputSelectionSupport;
     private ItemSelectionSupport outputSelectionSupport;
+    private static final int OPEN_DRAWER = 0;
+    private static final int CLOSE_DRAWER = 1;
     Port currentPort;
 
-    Handler handler=new Handler(){
+    Visualizer visualizer;
+    MediaPlayer mediaPlayer;
+
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
-                case 0:
+            switch (msg.what) {
+                case OPEN_DRAWER:
                     drawerLayout.openDrawer();
                     break;
-                case 1:
+                case CLOSE_DRAWER:
                     drawerLayout.closeDrawer();
                     break;
             }
@@ -71,70 +71,43 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_audio, container, false);
         ButterKnife.bind(this, v);
-
         return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Set<Channel> channelSet=presenter.getChannelSet();
-        List<Port> inputs=presenter.getInputList();
-        final List<Port> outputs=presenter.getOutputList();
-        //PortHelper.getInstance().init(inputs,outputs,channelSet);
-        inputSelectionSupport=new ItemSelectionSupport(inputRecyclerView);
-        outputSelectionSupport=new ItemSelectionSupport(outputRecyclerView);
+        inputSelectionSupport = new ItemSelectionSupport(inputRecyclerView);
+        outputSelectionSupport = new ItemSelectionSupport(outputRecyclerView);
         inputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.SINGLE);
         outputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.SINGLE);
-        inputAdapter=new VolumeAdapter(inputs,inputSelectionSupport);
-        outputAdapter=new VolumeAdapter(outputs,outputSelectionSupport);
-        inputRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),4));
-        outputRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),4));
+        inputAdapter = new VolumeAdapter(presenter.getInputList(), inputSelectionSupport);
+        outputAdapter = new VolumeAdapter(presenter.getOutputList(), outputSelectionSupport);
+        inputRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
+        outputRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
         inputRecyclerView.setHasFixedSize(true);
         outputRecyclerView.setHasFixedSize(true);
         inputRecyclerView.setAdapter(inputAdapter);
         outputRecyclerView.setAdapter(outputAdapter);
-        drawerLayout.setOnEditCompleteListener(new AssistDrawerLayout.OnEditCompleteListener() {
+        drawerLayout.setOnEditCompleteListener(new AudioDrawerLayout.OnEditCompleteListener() {
             @Override
-            public void onComplete(int mode) {
+            public void onComplete() {
                 //当前选中输入端
                 int in = inputSelectionSupport.getCheckedItemPosition();
                 //当前选中输出端
-                List<Integer> selects= outputSelectionSupport.getCheckedItemPositions();
-                int[] outs=new int[selects.size()];
-                for(int i=0;i<selects.size();i++){
-                    outs[i]=selects.get(i);
+                List<Integer> selects = outputSelectionSupport.getCheckedItemPositions();
+                int[] outs = new int[selects.size()];
+                for (int i = 0; i < selects.size(); i++) {
+                    outs[i] = selects.get(i);
                 }
-/*                switch (mode){
-                    case AssistDrawerLayout.MODE_1XN:
-                        presenter.switchVideo(in,outs);
-                        break;
-                    case AssistDrawerLayout.MODE_2X1:
-                        presenter.stitchVideo(in,2,1,outs);
-                        break;
-                    case AssistDrawerLayout.MODE_2X2:
-                        presenter.stitchVideo(in,2,2,outs);
-                        break;
-                    case AssistDrawerLayout.MODE_3X3:
-                        presenter.stitchVideo(in,3,3,outs);
-                        break;
-                }*/
-                presenter.setChannel(in,outs,Channel.MOD_NORMAL);
-                //编辑完成后设为单选模式
-                outputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.SINGLE);
-                outputAdapter.notifyDataSetChanged();
-                inputAdapter.notifyDataSetChanged();
-                //还原输出端选择的颜色和角标
-                outputSelectionSupport.initChoiceConfig(null);
-                //关闭抽屉，直接调用drawerLayout.closeDrawer()方法没有收回效果
-                handler.sendEmptyMessage(1);
+                presenter.switchVideo(in, outs);
+                editComplete();
             }
         });
 
-        inputRecyclerView.addOnItemTouchListener(new OnRecyclerItemClickListener(inputRecyclerView){
+        inputRecyclerView.addOnItemTouchListener(new OnRecyclerItemClickListener(inputRecyclerView) {
             @Override
             public void onItemClick(RecyclerView.ViewHolder vh, int position) {
                 inputSelectionSupport.itemClick(position);
@@ -144,7 +117,7 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
 
             @Override
             public void onLongClick(RecyclerView.ViewHolder vh, final int position) {
-                if(inputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.SINGLE&&outputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.SINGLE){
+                if (inputSelectionSupport.getChoiceMode() == ItemSelectionSupport.ChoiceMode.SINGLE && outputSelectionSupport.getChoiceMode() == ItemSelectionSupport.ChoiceMode.SINGLE) {
                     //获取系统震动服务
                     Vibrator vib = (Vibrator) getActivity().getSystemService(Service.VIBRATOR_SERVICE);
                     //震动70毫秒
@@ -152,11 +125,11 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
                     //输入端输出端都为单选模式
                     //初始化输出端选择的颜色和角标
                     outputSelectionSupport.initChoiceConfig(inputAdapter.getData().get(position));
-                    if(inputSelectionSupport.isItemChecked(position)){
+                    if (inputSelectionSupport.isItemChecked(position)) {
                         //长按的item已经被选中
                         outputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.MULTIPLE);
                         outputAdapter.notifyDataSetChanged();
-                    }else{
+                    } else {
                         //长按的item还未被选中
                         outputSelectionSupport.itemLongClick(-1);
                         inputSelectionSupport.itemClick(position);
@@ -164,10 +137,10 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
                     }
                     //弹出抽屉，直接调用drawerLayout.openDrawer()方法没有弹出效果
                     handler.sendEmptyMessage(0);
-                }else if(inputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.MULTIPLE){
+                } else if (inputSelectionSupport.getChoiceMode() == ItemSelectionSupport.ChoiceMode.MULTIPLE) {
                     //输入端为多选模式，输出端为单选模式
                     inputSelectionSupport.itemLongClick(position);
-                }else{
+                } else {
                     //输入端为单选模式，输出端为多选模式
                     inputSelectionSupport.itemClick(position);
                 }
@@ -178,34 +151,11 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
             }
         });
 
-        outputRecyclerView.addOnItemTouchListener(new OnRecyclerItemClickListener(outputRecyclerView){
+        outputRecyclerView.addOnItemTouchListener(new OnRecyclerItemClickListener(outputRecyclerView) {
             @Override
             public void onItemClick(RecyclerView.ViewHolder vh, int position) {
                 outputSelectionSupport.itemClick(position);
             }
-
-/*            @Override
-            public void onLongClick(RecyclerView.ViewHolder vh, int position) {
-                updateInfoBefore();
-                if(inputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.SINGLE&&outputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.SINGLE){
-                    if(outputSelectionSupport.isItemChecked(position)){
-                        //长按的item已经被选中
-                        inputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.MULTIPLE);
-                        inputAdapter.notifyDataSetChanged();
-                    }else{
-                        //长按的item还未被选中
-                        inputSelectionSupport.itemLongClick(-1);
-                        outputSelectionSupport.itemClick(position);
-                    }
-                }else if(outputSelectionSupport.getChoiceMode()== ItemSelectionSupport.ChoiceMode.MULTIPLE){
-                    //输入端为单选模式，输出端为多选模式
-                    outputSelectionSupport.itemLongClick(position);
-                }else{
-                    //输入端为多选模式，输出端为单选模式
-                    outputSelectionSupport.itemClick(position);
-                }
-                updateInfoAfter();
-            }*/
 
             @Override
             public void onItemDoubleClick(RecyclerView.ViewHolder vh, int position) {
@@ -215,11 +165,11 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
         inputSelectionSupport.setOnItemStatueListener(new ItemSelectionSupport.OnItemStatueListener() {
             @Override
             public void onSelectSingle(int position) {
-                currentPort=presenter.getInputList().get(position);
+                currentPort = presenter.getInputList().get(position);
                 //获取到选中输入端对应的输出端
-                int[] outsIdx=presenter.getOutputIdx(presenter.getInputList().get(position).idx);
+                int[] outsIdx = presenter.getOutputIdx(presenter.getInputList().get(position).idx);
                 outputSelectionSupport.clearChoices();
-                if(outsIdx!=null) {
+                if (outsIdx != null) {
                     for (int outIdx : outsIdx) {
                         //将对应的输出端设为选中状态
                         outputSelectionSupport.setItemChecked(outIdx, true);
@@ -230,7 +180,7 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
 
             @Override
             public void onUnSelectSingle(int position) {
-                currentPort=null;
+                currentPort = null;
                 //清空输出端列表的所有选中状态
                 outputSelectionSupport.clearChoices();
                 outputAdapter.notifyDataSetChanged();
@@ -245,46 +195,36 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
             public void onUnSelectMultiple(int position) {
 
             }
-
-/*            @Override
-            public void onPopupDialog(int position) {
-                //popupDialog(presenter.getInputList().get(position));
-            }
-
-            @Override
-            public void onSelectCountChange(int count) {
-            }*/
         });
 
         outputSelectionSupport.setOnItemStatueListener(new ItemSelectionSupport.OnItemStatueListener() {
             @Override
             public void onSelectSingle(int position) {
                 //获取选中输出端对应的输入端
-                int inIdx=presenter.getInputIdx(presenter.getOutputList().get(position).idx);
+                int inIdx = presenter.getInputIdx(presenter.getOutputList().get(position).idx);
                 inputSelectionSupport.clearChoices();
                 //将对应的输入端设为选中状态
-                if(inIdx!=-1) {
+                if (inIdx != -1) {
                     inputSelectionSupport.setItemChecked(inIdx, true);
                     inputAdapter.notifyDataSetChanged();
                     //获取对应输入端所对应的所有输出端
-                    int[] outsIdx=presenter.getOutputIdx(inIdx);
+                    int[] outsIdx = presenter.getOutputIdx(inIdx);
                     outputSelectionSupport.clearChoices();
                     //将对应的输出端设为选中状态
-                    if(outsIdx!=null) {
+                    if (outsIdx != null) {
                         for (int outIdx : outsIdx) {
                             outputSelectionSupport.setItemChecked(outIdx, true);
                         }
                     }
                     outputAdapter.notifyDataSetChanged();
-                }else{
+                } else {
                     inputAdapter.notifyDataSetChanged();
                 }
-
             }
 
             @Override
             public void onUnSelectSingle(int position) {
-                currentPort=null;
+                currentPort = null;
                 //清空输入端列表所有选中状态
                 inputSelectionSupport.clearChoices();
                 inputAdapter.notifyDataSetChanged();
@@ -299,16 +239,46 @@ public class AudioFragment extends BaseFragment implements AudioContract.View{
             public void onUnSelectMultiple(int position) {
 
             }
+        });
+        //因为矩阵预览卡流媒体只有视频没有音频，所以音频频谱暂时不用
+        //initVisualizer();
+        //visualizer.setEnabled(true);
+        //播放测试音频
+        //mediaPlayer.start();
+    }
 
-/*            @Override
-            public void onPopupDialog(int position) {
-                //popupDialog(presenter.getOutputList().get(position));
+    private void initVisualizer() {
+        //测试音频
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.test);
+        visualizer = new Visualizer(mediaPlayer.getAudioSessionId());
+        visualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        visualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            @Override
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
+
             }
 
             @Override
-            public void onSelectCountChange(int count) {
-            }*/
-        });
+            public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+                visualizerView.updateVisualizer(fft);
+            }
+        }, Visualizer.getMaxCaptureRate() / 2, false, true);
+    }
+
+    public AudioDrawerLayout getDrawerLayout() {
+        return drawerLayout;
+    }
+
+    /*设置完端口切换模式后调用*/
+    public void editComplete(){
+        //编辑完成后设为单选模式
+        outputSelectionSupport.setChoiceMode(ItemSelectionSupport.ChoiceMode.SINGLE);
+        outputAdapter.notifyDataSetChanged();
+        inputAdapter.notifyDataSetChanged();
+        //还原输出端选择的颜色和角标
+        outputSelectionSupport.initChoiceConfig(null);
+        //关闭抽屉，直接调用drawerLayout.closeDrawer()方法没有收回效果
+        handler.sendEmptyMessage(CLOSE_DRAWER);
     }
 
     @Override
