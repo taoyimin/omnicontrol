26249fb0a9797bdc0fc973fabebf541ab0d23e26
taoyimin <1103995288@qq.com>
2017-09-19 13:44:40 +0800

    log bean

---
+++ app/src/main/java/cn/diaovision/omnicontrol/core/model/device/common/DeviceLog.java
@@ -0,0 +1,21 @@
+package cn.diaovision.omnicontrol.core.model.device.common;
+
+/**
+ * Created by TaoYimin on 2017/9/19.
+ */
+
+public class DeviceLog {
+    String log;
+
+    public DeviceLog(String log) {
+        this.log = log;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+}
